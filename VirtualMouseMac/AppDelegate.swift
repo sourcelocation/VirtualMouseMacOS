594c2694c61acc4511577b33bfe636a7fe9e2e88
Matthew Anisovich <anisovich.matthew@gmail.com>
2021-11-02 21:03:54 +0300

    Initial Commit

---
+++ VirtualMouseMac/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  VirtualMouseMac
+//
+//  Created by exerhythm on 11/2/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ VirtualMouseMac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VirtualMouseMac
+//
+//  Created by exerhythm on 11/2/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
